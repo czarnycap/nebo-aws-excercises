177dde822d9a2c9349c5c2887684610fa962105f
Ian Oginski <Ian.Oginski@protonmail.com>
2023-04-04 13:04:33 +0200

    improvement: create policy for accessing s3 object (stored as variable)

--- iam_roles_and_users/main.tf
+++ iam_roles_and_users/main.tf
@@ -1,3 +1,8 @@
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to apply the policy to."
+  default = "adam1985bucket"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -69,22 +74,51 @@ resource "aws_iam_role" "rw-role" {
 #   })
 }
 
+# attach policy to the role
 resource "aws_iam_role_policy_attachment" "ro-role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   role       = aws_iam_role.ro-role.name
 }
 
+# attach policy to the role
 resource "aws_iam_role_policy_attachment" "rw-role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
   role       = aws_iam_role.rw-role.name
 }
 
-resource "aws_iam_user_policy_attachment" "ro-user_attachment" {
-  policy_arn = aws_iam_role_policy_attachment.ro-role_attachment.policy_arn
-  user       = aws_iam_user.ro-user.name
+# resource "aws_iam_user_policy_attachment" "ro-user_attachment" {
+#   policy_arn = aws_iam_role_policy_attachment.ro-role_attachment.policy_arn
+#   user       = aws_iam_user.ro-user.name
+# }
+
+# resource "aws_iam_user_policy_attachment" "rw-user_attachment" {
+#   policy_arn = aws_iam_role_policy_attachment.rw-role_attachment.policy_arn
+#   user       = aws_iam_user.rw-user.name
+# }
+
+
+
+
+resource "aws_s3_bucket_policy" "rw_bucket_policy" {
+  bucket = var.s3_bucket_name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.s3_bucket_name}/*",
+          "arn:aws:s3:::${var.s3_bucket_name}"
+        ]
+      }
+    ]
+  })
 }
 
-resource "aws_iam_user_policy_attachment" "rw-user_attachment" {
-  policy_arn = aws_iam_role_policy_attachment.rw-role_attachment.policy_arn
-  user       = aws_iam_user.rw-user.name
-}
